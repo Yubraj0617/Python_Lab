a236e8be3db3c6b139736e1dfe99563a4f079aee
Yubraj <yuvrajsapkota21@gmail.com>
2025-08-03 23:42:05 +0545

    Add files of add_divide , Area,Swap

---
+++ Add_Divide.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter a Number: "))
+num2 = int(input("Enter a Number: "))
+
+add = num1 + num2
+divide = num1 // num2  # Integer division
+
+
+print("Sum of two number: ",add)
+print("Divide of two number: ",divide)
---
+++ Area_Triangle.py
@@ -0,0 +1,5 @@
+lenght=float(input("Enter lenght of triangle: "));
+height=float(input("Enter height of triangle: "));
+area=0.5*lenght*height;
+print("Area of triangle is: ",area);
+
